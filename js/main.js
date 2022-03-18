0dda1e40140ee6ae857580a3f3a8ef3051062bec
Le Thanh Son <darkstar.son2k4@gmail.com>
2022-03-18 21:10:23 +0700

    fix hidden, show nav in mobile

--- js/main.js
+++ js/main.js
@@ -1,25 +1,30 @@
-        const buyBtns = document.querySelectorAll('.js-buy-ticket')
-        const modal = document.querySelector('.js-modal')
-        const btnPay = document.querySelector('.js-pay')
-        const closes = document.querySelectorAll('.js-close')
-        const containerModal = document.querySelector('.js-container-modal')
-        function showModal() {
-            modal.classList.add('open') 
-        }
-        console.log(buyBtns);
-        for(const buyBtn of buyBtns) {
-            buyBtn.addEventListener('click', showModal)
-        }
-        function hideModal() {
-            modal.classList.remove('open')
-        }
-        for(const close of closes) {
-            close.addEventListener('click', hideModal)
-        }
-        modal.addEventListener('click', hideModal)
-        containerModal.addEventListener('click', function (even) {
-            even.stopPropagation()
-        })
-        btnPay.addEventListener('click', function(even) {
-            alert("Have a good day, you bought the tickets")
-        })
+const headerId = document.querySelector('#header')
+const btnMenuMobile = document.querySelector('.js-menu-btn')
+const buyBtns = document.querySelectorAll('.js-buy-ticket')
+const modal = document.querySelector('.js-modal')
+const btnPay = document.querySelector('.js-pay')
+const closes = document.querySelectorAll('.js-close')
+const containerModal = document.querySelector('.js-container-modal')
+function showModal() {
+    modal.classList.add('open') 
+}
+console.log(buyBtns);
+for(const buyBtn of buyBtns) {
+    buyBtn.addEventListener('click', showModal)
+}
+function hideModal() {
+    modal.classList.remove('open')
+}
+for(const close of closes) {
+    close.addEventListener('click', hideModal)
+}
+modal.addEventListener('click', hideModal)
+containerModal.addEventListener('click', function (even) {
+    even.stopPropagation()
+})
+btnPay.addEventListener('click', function(even) {
+    alert("Have a good day, you bought the tickets")
+})
+btnMenuMobile.onclick = function() {
+    headerId.classList.toggle("openInMobile");
+}
