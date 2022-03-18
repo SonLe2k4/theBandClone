926e225aa21f5d64e57d2fbc509c0c6028ef791f
Le Thanh Son <darkstar.son2k4@gmail.com>
2022-03-18 20:06:59 +0700

    upload sourse pj

---
+++ js/main.js
@@ -0,0 +1,24 @@
+        const buyBtns = document.querySelectorAll('.js-buy-btn')
+        const modal = document.querySelector('.js-modal')
+        const btnPay = document.querySelector('.js-pay')
+        const closes = document.querySelectorAll('.js-close')
+        const containerModal = document.querySelector('.js-container-modal')
+        function showModal() {
+            modal.classList.add('open') 
+        }
+        for(const buyBtn of buyBtns) {
+            buyBtn.addEventListener('click', showModal)
+        }
+        function hideModal() {
+            modal.classList.remove('open')
+        }
+        for(const close of closes) {
+            close.addEventListener('click', hideModal)
+        }
+        modal.addEventListener('click', hideModal)
+        containerModal.addEventListener('click', function (even) {
+            even.stopPropagation()
+        })
+        btnPay.addEventListener('click', function(even) {
+            alert("Have a good day, you bought the tickets")
+        })
